9dc7030863421d4f5d5bc9722a574f8bef2c2ff0
Mayank <mayank8630bisht@gmail.com>
2023-05-15 21:23:51 +0530

    Designed Home Page

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,20 @@ import Portfolio from "./containers/portfolio";
 import Contact from "./containers/contact";
 import NavBar from "./components/navBar";
 import {BrowserRouter} from 'react-router-dom'
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
+import particles from "./utills.js/particles";
 
 function App() {
+  const handleInit=async (main)=>{
+    await loadFull(main)
+  }
   return (
     <>
       <div className="App">
         <BrowserRouter>
           {/*particles*/}
+          <Particles id="particles" options={particles} init={handleInit} />
 
           {/*navar*/}
           <NavBar />
