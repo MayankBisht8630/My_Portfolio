bab0ab1c36957942bfc7eb332f0256176c81bb8f
Mayank <mayank8630bisht@gmail.com>
2023-05-16 19:20:34 +0530

    made skill page

--- src/containers/skills/index.jsx
+++ src/containers/skills/index.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { BsInfoCircleFill } from "react-icons/bs";
 import PageHeaderContent from "../../components/pageHeaderContent";
+import './util.js';
+import { skillData } from "./util.js";
 
 const Skills = () => {
   return (
@@ -9,6 +11,37 @@ const Skills = () => {
         headerText="My Skills "
         icon={<BsInfoCircleFill size={40} />}
       />
+
+      <div className="skills__content-wrapper">
+        {skillData.map((item, i) => (
+          <div key={i} className="skills__content-wrapper__inner-content">
+            <Animate
+              play
+              duration={1}
+              delay={0.3}
+              start={{
+                transform: "translateX(-200px)",
+              }}
+              end={{
+                transform: "translatex(0px)",
+              }}
+            >
+              <h3 className="skills__content-wrapper__inner-content__category-text">
+                {item.label}
+              </h3>
+              <div>
+                {
+                  item.data.map((skillItem)=>(
+                    <AnimateKeyframes>
+                      
+                    </AnimateKeyframes>
+                  ))
+                }
+              </div>
+            </Animate>
+          </div>
+        ))}
+      </div>
     </section>
   );
 };
---
+++ src/containers/skills/util.js
@@ -0,0 +1,65 @@
+export const skillData = [
+  {
+    label: "FRONT END",
+    data: [
+      {
+        skillName: "REACT JS",
+        percentage: "80",
+      },
+      {
+        skillName: "HTML",
+        percentage: "90",
+      },
+      {
+        skillName: "CSS",
+        percentage: "70",
+      },
+      {
+        skillName: "JAVASCRIPT",
+        percentage: "80",
+      },
+    ],
+  },
+  {
+    label: "BACK END",
+    data: [
+      {
+        skillName: "JAVA",
+        percentage: "90",
+      },
+      {
+        skillName: "PHP",
+        percentage: "80",
+      },
+      {
+        skillName: "C",
+        percentage: "60",
+      },
+      {
+        skillName: "PYTHON",
+        percentage: "50",
+      },
+    ],
+  },
+  {
+    label: "FRAMEWORKS AND DATABASES",
+    data: [
+      {
+        skillName: "SPRING FRAMEWORK",
+        percentage: "90",
+      },
+      {
+        skillName: "SPRING BOOT FRAMEWORK",
+        percentage: "80",
+      },
+      {
+        skillName: "LARAVEL FRAMEWORK",
+        percentage: "60",
+      },
+      {
+        skillName: "MY SQL",
+        percentage: "90",
+      },
+    ],
+  },
+];
