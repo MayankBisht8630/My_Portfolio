634d8cf5a7fc51540657cfb7b202fa8dd0cd0ada
Mayank <mayank8630bisht@gmail.com>
2023-05-17 15:56:50 +0530

    created resume page

--- src/containers/resume/index.jsx
+++ src/containers/resume/index.jsx
@@ -1,6 +1,15 @@
 import React from "react";
+import "./styles.scss";
 import PageHeaderContent from "../../components/pageHeaderContent";
 import { BsInfoCircleFill } from "react-icons/bs";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import { data } from "./util";
+import "./util.js";
+import { MdWork } from "react-icons/md";
+import "react-vertical-timeline-component/style.min.css";
 
 const Resume = () => {
   return (
@@ -9,6 +18,73 @@ const Resume = () => {
         headerText="My Resume"
         icon={<BsInfoCircleFill size={40} />}
       />
+      <div className="timeline">
+        <div className="timeline__experience">
+          <h3 className="timeline__experience__header-text">EXPERIENCE</h3>
+          <VerticalTimeline
+            layout={"1-column"}
+            lineColor="var(--yellow-theme-main-color)"
+          >
+            {data.experience.map((item, i) => (
+              <VerticalTimelineElement
+                key={i}
+                className="timeline__experience__vertical-timeline-element"
+                contentStyle={{
+                  background: "none",
+                  color: "var(--yellow-theme-sub-text-color)",
+                  border: "1.5px solid var(--yellow-theme-main-color)",
+                }}
+                date="2022 - Present"
+                icon={<MdWork />}
+                iconStyle={{
+                  background: "#181818",
+                  color: "var(--yellow-theme-main-color)",
+                }}
+              >
+                <div className="vertical-timeline-element-title-wrapper">
+                  <h3>{item.title}</h3>
+                </div>
+                <h4>{item.subTitle}</h4>
+                <p className="vertical-timeline-element-title-wrapper-description">
+                  {item.description}
+                </p>
+              </VerticalTimelineElement>
+            ))}
+          </VerticalTimeline>
+        </div>
+        <div className="timeline__education">
+          <h3 className="timeline__education__header-text">EDUCATION</h3>
+          <VerticalTimeline
+            layout={"1-column"}
+            lineColor="var(--yellow-theme-main-color)"
+          >
+            {data.education.map((item, i) => (
+              <VerticalTimelineElement
+                key={i}
+                className="timeline__experience__vertical-timeline-element"
+                contentStyle={{
+                  background: "none",
+                  color: "var(--yellow-theme-sub-text-color)",
+                  border: "1.5px solid var(--yellow-theme-main-color)",
+                }}
+                icon={<MdWork />}
+                iconStyle={{
+                  background: "#181818",
+                  color: "var(--yellow-theme-main-color)",
+                }}
+              >
+                <div className="vertical-timeline-element-title-wrapper">
+                  <h3>{item.title}</h3>
+                  <h4>{item.subTitle}</h4>
+                </div>
+                <p className="vertical-timeline-element-title-wrapper-description">
+                  {item.description}
+                </p>
+              </VerticalTimelineElement>
+            ))}
+          </VerticalTimeline>
+        </div>
+      </div>
     </section>
   );
 };
